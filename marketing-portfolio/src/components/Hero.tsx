d6f310ca08905d176249e23d7ba00d606ee28169
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-10-25 17:27:13 +0200

    setup placeholder img, heading and gradient for hero section

--- marketing-portfolio/src/components/Hero.tsx
+++ marketing-portfolio/src/components/Hero.tsx
@@ -1,8 +1,10 @@
+import Image from "next/image";
 import React from "react";
 
 const Hero = () => {
   return (
-    <section className="max-container padding-container border-2 flex flex-col border-red-500 gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row">
+    <section className="bg-gradient-to-r from-slate-500 max-container padding-container border-2 flex flex-col border-red-500 gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row">
+      <Image className="self-center" src="/expert2.jpg" width={350} height={350} />
       <h1 className="bold-52 lg:bold-88">Social media ekspert</h1>
       <p className="regular-16 mt-6 text-gray-30 xl:max-w-[520px]">
         Ogarnij se social media, cokolwiek robisz to barti ogarnie, bedzie git.
