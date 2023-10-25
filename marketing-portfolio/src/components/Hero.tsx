9f434ef93cf14c5af0d81b5d176f7096174036a9
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-10-25 17:13:28 +0200

    add random paragraph filling, style hero section with header and paragraph text

--- marketing-portfolio/src/components/Hero.tsx
+++ marketing-portfolio/src/components/Hero.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section className="max-container padding-container border-2 flex flex-col border-red-500 gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row">
+      <h1 className="bold-52 lg:bold-88">Social media ekspert</h1>
+      <p className="regular-16 mt-6 text-gray-30 xl:max-w-[520px]">
+        Ogarnij se social media, cokolwiek robisz to barti ogarnie, bedzie git.
+        Marketing jest super wazny bo wiadomo. Jak cos to wiecej info na dole,
+        tutaj tylko wstep. Bedzie spoko strona z tego.
+      </p>
+    </section>
+  );
 };
 
 export default Hero;
