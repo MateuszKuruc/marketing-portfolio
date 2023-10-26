4c63163e10de2438aa9ea411c2a670763b12f2e1
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-10-26 15:18:49 +0200

    change border styling of hero and about sections

--- marketing-portfolio/src/components/About.tsx
+++ marketing-portfolio/src/components/About.tsx
@@ -1,28 +1,34 @@
+import Image from "next/image";
 import React from "react";
 
 const About = () => {
-  return(
-    <section className="mt-10 skew-y-3 bg-gradient-to-b from-gray-90 max-container padding-container py-10 md:gap-28 lg:py-20">
-<div className="-skew-y-3">
-<h1 className="bold-52 lg:bold-70 text-blue-50">O mnie</h1>
-<p className="regular-16 mt-6 text-gray-30 xl:max-w-[520px] text-white">Jestem fajowym marketingowcem, zrobie ci graficzke i zmontuje teksty reklamowe. Zajmuje sie tym 20 lat co najmniej. Co bys jeszcze chcial? Robie:
- 
-</p>
-<ul className="list-disc text-white">
-    <li>
-      kampanie fb
-    </li>
-    <li>
-      prowadze social media fb i ig
-    </li>
-    <li>
-      prowadze tysiac rodzajow kampanii w necie
-    </li>
-    <li>kompleksowo ogarne ci marketing</li>
-  </ul>
-</div>
+  return (
+    <section className="border-t-4 border-white my-10 skew-y-3 bg-gradient-to-b from-gray-90 max-container padding-container py-10 md:gap-28 lg:py-20 gap-20">
+      <div className="-skew-y-3 flex gap-10 padding-container">
+        <header>
+          <h1 className="bold-52 lg:bold-70 text-blue-50">O mnie</h1>
+          <p className="regular-16 mt-6 text-gray-30 xl:max-w-[520px] text-white">
+            Jestem fajowym marketingowcem, zrobie ci graficzke i zmontuje teksty
+            reklamowe. Zajmuje sie tym 20 lat co najmniej. Co bys jeszcze
+            chcial? Robie:
+          </p>
+          <ul className="list-disc text-white">
+            <li className="mt-3">kampanie fb</li>
+            <li>prowadze social media fb i ig</li>
+            <li>prowadze tysiac rodzajow kampanii w necie</li>
+            <li>kompleksowo ogarne ci marketing</li>
+          </ul>
+        </header>
+        {/* <Image
+          className="lg:block md:hidden sm:hidden xs:hidden"
+          src="/placeholder.jpg"
+          width={350}
+          height={350}
+          alt="marketing photo"
+        /> */}
+      </div>
     </section>
-  )
+  );
 };
 
 export default About;
--- marketing-portfolio/src/components/Portfolio.tsx
+++ marketing-portfolio/src/components/Portfolio.tsx
@@ -2,7 +2,8 @@ import React from "react";
 
 const Portfolio = () => {
   return (
-    <section className="mt-10 max-container padding-container bg-slate-300 py-10 lg:py-20 md:gap-28 mt-50">
+    // <section className=" max-container padding-container bg-gradient-to-t from-gray-90 py-10 lg:py-20 md:gap-28">
+    <section className="bg-red-50 mt-50 border-2 border-white">
       <h1 className="bold-52 lg:bold-70">Portfolio</h1>
       <p>Sprawdz jak poprawilem marketingowe wskazniki u moich klientow </p>
     </section>
@@ -10,3 +11,7 @@ const Portfolio = () => {
 };
 
 export default Portfolio;
+
+
+
+// divide border for projects? with accordion? first one shown
