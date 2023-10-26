90ca3ebcc75dbfa3f4f875acafe6a4a53aa976fd
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-10-26 14:43:50 +0200

    fix desktop and mobile styling in hero section

--- marketing-portfolio/src/components/Hero.tsx
+++ marketing-portfolio/src/components/Hero.tsx
@@ -4,23 +4,19 @@ import React from "react";
 const Hero = () => {
   return (
     <section className="skew-y-3 bg-gradient-to-t from-gray-90 max-container padding-container flex flex-col flexCenter gap-20 py-10 pb-32 md:gap-28 lg:py-20">
-      <div className="-skew-y-3">
-
-      <Image
-        className="self-center"
-        src="/expert2.jpg"
-        width={350}
-        height={350}
-        alt="expert photo"
-        />
-      <h1 className="mt-6 bold-52 lg:bold-70 text-blue-50">Social media ekspert</h1>
-      <p className="regular-16 mt-6 text-white xl:max-w-[520px]">
-        Ogarnij se social media, cokolwiek robisz to barti ogarnie, bedzie git.
-        Marketing jest super wazny bo wiadomo. Jak cos to wiecej info na dole,
-        tutaj tylko wstep. Bedzie spoko strona z tego.
-      </p>
-        </div>
-    
+      <div className="-skew-y-3 lg:flex gap-10 ">
+        <Image src="/expert2.jpg" width={350} height={350} alt="expert photo" />
+        <header>
+          <h1 className="mt-6 bold-52 lg:bold-70 text-blue-50">
+            Social media ekspert
+          </h1>
+          <p className="regular-16 mt-6 text-white xl:max-w-[520px]">
+            Ogarnij se social media, cokolwiek robisz to barti ogarnie, bedzie
+            git. Marketing jest super wazny bo wiadomo. Jak cos to wiecej info
+            na dole, tutaj tylko wstep. Bedzie spoko strona z tego.
+          </p>
+        </header>
+      </div>
     </section>
   );
 };
