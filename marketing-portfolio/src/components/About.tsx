6d9618788da6650d76e1e165da8ca310e1f2635f
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-10-26 12:54:57 +0200

    add basic styling and placeholder content for about page

--- marketing-portfolio/src/components/About.tsx
+++ marketing-portfolio/src/components/About.tsx
@@ -1,7 +1,28 @@
 import React from "react";
 
 const About = () => {
-  return <div>About</div>;
+  return(
+    <section className="skew-y-3 bg-gradient-to-r from-orange-50 max-container padding-container border-2 border-green-500 py-10 md:gap-28 lg:py-20">
+<div className="-skew-y-3">
+<h1 className="bold-52 lg:bold-70">O mnie</h1>
+<p>Jestem fajowym marketingowcem, zrobie ci graficzke i zmontuje teksty reklamowe. Zajmuje sie tym 20 lat co najmniej. Co bys jeszcze chcial? Robie:
+  <ul className="list-disc">
+    <li>
+      kampanie fb
+    </li>
+    <li>
+      prowadze social media fb i ig
+    </li>
+    <li>
+      prowadze tysiac rodzajow kampanii w necie
+    </li>
+    <li>kompleksowo ogarne ci marketing</li>
+  </ul>
+</p>
+
+</div>
+    </section>
+  )
 };
 
 export default About;
--- marketing-portfolio/src/components/Hero.tsx
+++ marketing-portfolio/src/components/Hero.tsx
@@ -3,14 +3,24 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <section className="bg-gradient-to-r from-slate-500 max-container padding-container border-2 flex flex-col border-red-500 gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row">
-      <Image className="self-center" src="/expert2.jpg" width={350} height={350} />
-      <h1 className="bold-52 lg:bold-88">Social media ekspert</h1>
+    <section className="skew-y-3 bg-gradient-to-r from-blue-50 max-container padding-container border-2 flex flex-col border-red-500 gap-20 py-10 pb-32 md:gap-28 lg:py-20">
+      <div className="-skew-y-3">
+
+      <Image
+        className="self-center"
+        src="/expert2.jpg"
+        width={350}
+        height={350}
+        alt="expert photo"
+        />
+      <h1 className="bold-52 lg:bold-70">Social media ekspert</h1>
       <p className="regular-16 mt-6 text-gray-30 xl:max-w-[520px]">
         Ogarnij se social media, cokolwiek robisz to barti ogarnie, bedzie git.
         Marketing jest super wazny bo wiadomo. Jak cos to wiecej info na dole,
         tutaj tylko wstep. Bedzie spoko strona z tego.
       </p>
+        </div>
+    
     </section>
   );
 };
