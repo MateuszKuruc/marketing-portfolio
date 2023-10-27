5078392eac551d8477e580f34ded44d08d7d2be5
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-10-27 20:12:03 +0200

    adjust styling for contact component

--- marketing-portfolio/src/components/Contact.tsx
+++ marketing-portfolio/src/components/Contact.tsx
@@ -6,24 +6,31 @@ const Contact = () => {
   return (
     <section className="border-t-4 bg-gradient-to-b from-gray-90 py-10 max-container padding-container">
       <h1 className="bold-52 lg:bold-70 text-blue-50">Kontakt</h1>
-      <p className="regular-20 text-white">
-        Uzupelnij formularz i daj znac czego potrzebujesz
-      </p>
-      <Form />
-      <p className="regular-20 text-white">...lub skontaktuj się przez:</p>
-      <p className="text-white regular-16">
-        Email:{" "}
-        <Link
-          className="text-blue-50"
-          href={`mailto:${process.env.CONTACT_EMAIL}`}
-        >
-          {process.env.CONTACT_EMAIL}
-        </Link>
-      </p>
-      <p className="text-white regular-16">
-        Telefon:{" "}
-        <span className="text-blue-50">{process.env.CONTACT_NUMBER}</span>
-      </p>
+      <div className="md:flexStart gap-20 sm:flexCenter">
+        <div>
+          <p className="regular-20 text-white">
+            Uzupelnij formularz i daj znac czego potrzebujesz
+          </p>
+          <Form />
+        </div>
+
+        <div className="sm: mt-10">
+          <p className="regular-20 text-white">...lub skontaktuj się przez:</p>
+          <p className="text-white regular-16">
+            Email:{" "}
+            <Link
+              className="text-blue-50"
+              href={`mailto:${process.env.CONTACT_EMAIL}`}
+            >
+              {process.env.CONTACT_EMAIL}
+            </Link>
+          </p>
+          <p className="text-white regular-16">
+            Telefon:{" "}
+            <span className="text-blue-50">{process.env.CONTACT_NUMBER}</span>
+          </p>
+        </div>
+      </div>
     </section>
   );
 };
