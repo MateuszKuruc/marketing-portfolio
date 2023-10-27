80af4470608b49117a24ed428f7228da3b17a592
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-10-27 13:30:06 +0200

    start implementing formik into form component

--- marketing-portfolio/src/components/Form.tsx
+++ marketing-portfolio/src/components/Form.tsx
@@ -1,7 +1,30 @@
+"use client";
+import { useFormik } from "formik";
 import React from "react";
 
 const Form = () => {
-  return <div>Form</div>;
+  const handleSubmit = (values, actions) => {
+    console.log("form submitted");
+  };
+
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+    },
+    onSubmit: handleSubmit,
+  });
+  return (
+    <form className="bg-gray-50 py-10" onSubmit={formik.handleSubmit}>
+      <label htmlFor="email">Email</label>
+      <input
+        value={formik.values.email}
+        onChange={formik.handleChange}
+        id="email"
+        type="email"
+        placeholder="Enter your email"
+      />
+    </form>
+  );
 };
 
 export default Form;
