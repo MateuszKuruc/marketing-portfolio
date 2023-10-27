58b965d239ecda64d776f382a83d62cac7c39a31
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-10-27 23:29:11 +0200

    change footer styling and content

--- marketing-portfolio/src/components/Footer.tsx
+++ marketing-portfolio/src/components/Footer.tsx
@@ -10,19 +10,36 @@ import {
 const Footer = () => {
   return (
     <footer className="flexCenter mb-0 bg-gray-90 py-10 max-container">
-      <div className="flex flex-col items-center gap-4">
-        <p className="text-blue-50 bold-20">Social</p>
-        <div className="flex ">
-
-        <Link href="placeholder">
-          <AiOutlineFacebook color="white" size="50" />
-        </Link>
-        <Link href="placeholderlink">
-          <AiOutlineInstagram color="white" size="50" />
-        </Link>
-        <Link href="placeholderlink">
-          <AiOutlineLinkedin color="white" size="50" />
-        </Link>
+      <div className="flex gap-12">
+        <div className="flex flex-col items-center gap-2">
+          <p className="text-blue-50 bold-20">Kontakt</p>
+          <p className="text-white regular-16">
+            Email:{" "}
+            <Link
+              className="text-blue-50"
+              href={`mailto:${process.env.CONTACT_EMAIL}`}
+            >
+              {process.env.CONTACT_EMAIL}
+            </Link>
+          </p>
+          <p className="text-white regular-16">
+            Telefon:{" "}
+            <span className="text-blue-50">{process.env.CONTACT_NUMBER}</span>
+          </p>
+        </div>
+        <div className="flex flex-col items-center gap-4">
+          <p className="text-blue-50 bold-20">Social</p>
+          <div className="flex ">
+            <Link href="placeholder">
+              <AiOutlineFacebook color="white" size="50" />
+            </Link>
+            <Link href="placeholderlink">
+              <AiOutlineInstagram color="white" size="50" />
+            </Link>
+            <Link href="placeholderlink">
+              <AiOutlineLinkedin color="white" size="50" />
+            </Link>
+          </div>
         </div>
       </div>
     </footer>
