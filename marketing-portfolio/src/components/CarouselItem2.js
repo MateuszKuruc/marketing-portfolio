87ab5a32112297d0aa0dd6faaef0f7bd25f4f2e4
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-10-30 19:48:04 +0100

    improve styling of carousel component

--- marketing-portfolio/src/components/CarouselItem2.js
+++ marketing-portfolio/src/components/CarouselItem2.js
@@ -12,38 +12,40 @@ const CarouselItem2 = () => {
   const responsive = {
     superLargeDesktop: {
       // the naming can be any, depends on you.
-      breakpoint: { max: 4000, min: 3000 },
-      items: 5,
+      breakpoint: { max: 4000, min: 1024 },
+      items: 6,
     },
     desktop: {
-      breakpoint: { max: 3000, min: 1024 },
-      items: 3,
+      breakpoint: { max: 1024, min: 800 },
+      items: 5,
     },
     tablet: {
-      breakpoint: { max: 1024, min: 464 },
-      items: 2,
+      breakpoint: { max: 800, min: 464 },
+      items: 3,
     },
     mobile: {
       breakpoint: { max: 464, min: 0 },
-      items: 1,
+      items: 2,
     },
   };
 
   return (
-    <div 
-    className="max-container padding-container bg-pink-50 p-10"
-    >
-      <h1 className="text-blue-50 bold-52 lg:bold-70 mb-5">Klienci, którzy mi zaufali:</h1>
-      <Carousel responsive={responsive}>
+    <div className="max-container padding-container bg-pink-50 p-10">
+      <h1 className="text-blue-50 bold-52 lg:bold-70 mb-5">
+        Klienci, którzy mi zaufali:
+      </h1>
+      <Carousel responsive={responsive} className="border-4 border-red-50">
         {CAROUSEL_DATA.map((item) => (
-          <div key={item.name}>
-            <Image src={item.img} width={200} height={200} alt={item.name} className="border-4 rounded-2xl border-blue-50" />
+          <div key={item.name} className="mx-1">
+            <Image
+              src={item.img}
+              width={200}
+              height={200}
+              alt={item.name}
+              className="border-4 border-blue-50 rounded-full"
+            />
           </div>
         ))}
-        {/* <div>Item 1</div>
-  <div>Item 2</div>
-  <div>Item 3</div>
-  <div>Item 4</div> */}
       </Carousel>
     </div>
   );
