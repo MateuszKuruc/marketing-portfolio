8b651684eec9273ce99737cd4e3edc4784aefb64
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-10-30 13:34:41 +0100

    remove react multi carousel library which was causing errors on runtime

--- marketing-portfolio/src/app/page.tsx
+++ marketing-portfolio/src/app/page.tsx
@@ -2,14 +2,13 @@ import About from "@/components/About";
 import Contact from "@/components/Contact";
 import Hero from "@/components/Hero";
 import Portfolio from "@/components/Portfolio";
-import CarouselItem from "@/components/CarouselItem";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <About />
-      <CarouselItem />
+
       <Portfolio />
       <Contact />
     </>
