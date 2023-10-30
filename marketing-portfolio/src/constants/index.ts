e87cc206b09e2efab6f13d6ce1752d638a0f1c3f
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-10-30 20:36:23 +0100

    clean code in costants folder

--- marketing-portfolio/src/constants/index.ts
+++ marketing-portfolio/src/constants/index.ts
@@ -1,18 +1,12 @@
 // NAVIGATION
 export const NAV_LINKS = [
   { href: "/", key: "home", label: "Strona główna" },
-  { href: "/", key: "case_study", label: "O mnie" },
-  { href: "/", key: "services", label: "Portfolio" },
-  { href: "/", key: "about ", label: "Kontakt " },
+  { href: "/", key: "about", label: "O mnie" },
+  { href: "/", key: "portfolio", label: "Portfolio" },
+  { href: "/", key: "contact ", label: "Kontakt " },
 ];
 
-//   export const NAV_LINKS = [
-//     { href: '/', key: 'home', label: 'Home' },
-//     { href: '/', key: 'how_hilink_work', label: 'How Hilink Work?' },
-//     { href: '/', key: 'services', label: 'Services' },
-//     { href: '/', key: 'pricing ', label: 'Pricing ' },
-//     { href: '/', key: 'contact_us', label: 'Contact Us' },
-//   ];
+// Clients for accordion
 
 export const ACCORDION_DATA = [
   {
@@ -31,6 +25,8 @@ export const ACCORDION_DATA = [
       "Prowadzilem konsultacje marketingowe dla klienta w branzy bankowej. Tematyka dotyczyla doboru grafik przy prowadzeniu profilow w social media. adasdasdsadsa sadasdasdsadas dsadsadsadsad sadsadsadsa",
   },
 ];
+
+// Logos for carousel
 
 export const CAROUSEL_DATA = [
   {
@@ -73,84 +69,4 @@ export const CAROUSEL_DATA = [
     name: "Okna Debow",
     img: "/logo11.png",
   },
- 
- 
 ];
-
-// CAMP SECTION
-export const PEOPLE_URL = [
-  "/person-1.png",
-  "/person-2.png",
-  "/person-3.png",
-  "/person-4.png",
-];
-
-// FEATURES SECTION
-export const FEATURES = [
-  {
-    title: "Real maps can be offline",
-    icon: "/map.svg",
-    variant: "green",
-    description:
-      "We provide a solution for you to be able to use our application when climbing, yes offline maps you can use at any time there is no signal at the location",
-  },
-  {
-    title: "Set an adventure schedule",
-    icon: "/calendar.svg",
-    variant: "green",
-    description:
-      "Schedule an adventure with friends. On holidays, there are many interesting offers from Hilink. That way, there's no more discussion",
-  },
-  {
-    title: "Technology using augment reality",
-    icon: "/tech.svg",
-    variant: "green",
-    description:
-      "Technology uses augmented reality as a guide to your hiking trail in the forest to the top of the mountain. Already supported by the latest technology without an internet connection",
-  },
-  {
-    title: "Many new locations every month",
-    icon: "/location.svg",
-    variant: "orange",
-    description:
-      "Lots of new locations every month, because we have a worldwide community of climbers who share their best experiences with climbing",
-  },
-];
-
-// FOOTER SECTION
-export const FOOTER_LINKS = [
-  {
-    title: "Learn More",
-    links: [
-      "About Hilink",
-      "Press Releases",
-      "Environment",
-      "Jobs",
-      "Privacy Policy",
-      "Contact Us",
-    ],
-  },
-  {
-    title: "Our Community",
-    links: ["Climbing xixixi", "Hiking hilink", "Hilink kinthill"],
-  },
-];
-
-export const FOOTER_CONTACT_INFO = {
-  title: "Contact Us",
-  links: [
-    { label: "Admin Officer", value: "123-456-7890" },
-    { label: "Email Officer", value: "hilink@akinthil.com" },
-  ],
-};
-
-export const SOCIALS = {
-  title: "Social",
-  links: [
-    "/facebook.svg",
-    "/instagram.svg",
-    "/twitter.svg",
-    "/youtube.svg",
-    "/wordpress.svg",
-  ],
-};
