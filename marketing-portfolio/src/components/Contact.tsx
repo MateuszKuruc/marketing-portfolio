20144d045c03623e9459383247636ce903a08112
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-10-30 01:39:15 +0100

    adjust form styling for mobile

--- marketing-portfolio/src/components/Contact.tsx
+++ marketing-portfolio/src/components/Contact.tsx
@@ -5,12 +5,15 @@ import Image from "next/image";
 
 const Contact = () => {
   return (
-    <section className="border-t-4 bg- py-0 max-container bg-radial-gradient">
-      <div className="flex flex-col md:items-center relative w-full">
-        <h1 className="bold-52 mt-8 lg:bold-70 text-blue-50">Kontakt</h1>
-        <p className="regular-20 text-white">
+    <section className="border-t-4 py-0 max-container bg-radial-gradient">
+      <div className="flex flex-col relative w-full md:items-center">
+        {/* <div className="flex flex-col items-center justify-center"> */}
+
+        <h1 className="self-center bold-52 mt-8 lg:bold-70 text-blue-50">Kontakt</h1>
+        <p className="p-4 regular-20 text-white ">
           Uzupelnij formularz i daj znac czego potrzebujesz
         </p>
+        {/* </div> */}
         <Image
           src="/background.jpg"
           className="w-full h-full absolute object-cover mix-blend-luminosity rounded-2xl opacity-5"
@@ -19,7 +22,7 @@ const Contact = () => {
           height={500}
           quality={100}
         />
-        <div>
+        <div className="pb-4 px-4">
           <Form />
         </div>
       </div>
