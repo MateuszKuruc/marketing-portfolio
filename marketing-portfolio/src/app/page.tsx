57c00498d6aa9388ff176ddf9019ab43d24bb2c0
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-10-30 20:23:36 +0100

    clean code across components

--- marketing-portfolio/src/app/page.tsx
+++ marketing-portfolio/src/app/page.tsx
@@ -2,14 +2,14 @@ import About from "@/components/About";
 import Contact from "@/components/Contact";
 import Hero from "@/components/Hero";
 import Portfolio from "@/components/Portfolio";
-import CarouselItem2 from "@/components/CarouselItem2";
+import CarouselItem from "@/components/CarouselItem";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <About />
-<CarouselItem2 />
+      <CarouselItem />
       <Portfolio />
       <Contact />
     </>
--- marketing-portfolio/src/components/CarouselItem.tsx
+++ marketing-portfolio/src/components/CarouselItem.tsx
@@ -1,42 +1,62 @@
-// "use client";
-// import React from "react";
-// import { CAROUSEL_DATA } from "@/constants";
-// import Slider from "react-slick";
-// import "slick-carousel/slick/slick.css";
-// import "slick-carousel/slick/slick-theme.css";
+"use client";
+import React from "react";
+import { CAROUSEL_DATA } from "@/constants";
 
-// import Image from "next/image";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import Image from "next/image";
 
-// const CarouselItem = () => {
-//   const settings = {
-//     dots: true,
-//     infinite: true,
-//     speed: 500,
-//     slidesToShow: 3,
-//     slidesToScroll: 1,
-//   };
+const CarouselItem = () => {
+  const responsive = {
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 1024 },
+      items: 5,
+    },
+    desktop: {
+      breakpoint: { max: 1024, min: 800 },
+      items: 5,
+    },
+    tablet: {
+      breakpoint: { max: 800, min: 464 },
+      items: 4,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 3,
+    },
+  };
 
-//   return (
-//     <div className="m-auto w-3/4">
-//       <div className="mt-20">
-//         <Slider {...settings}>
-//           {CAROUSEL_DATA.map((item) => (
-//             <div
-//               key={item.name}
-//                 // className="bg-white text-black rounded-xl w-[200px] h-[200px]"
-//             >
-//               {/* <img
-//                 src={item.img}
-//                 alt={item.name}
-//                 className="rounded-3xl bg-indigo-500 flex items-center justify-center border-4 border-blue-50"
-//               /> */}
-//               <Image src={item.img} width={200} height={200} alt={item.name} />
-//             </div>
-//           ))}
-//         </Slider>
-//       </div>
-//     </div>
-//   );
-// };
+  return (
+    <div className="max-container padding-container bg-pink-50 p-10">
+      <h1 className="text-blue-50 bold-52 lg:bold-70 mb-5">
+        Klienci, którzy mi zaufali:
+      </h1>
+      <Carousel
+        className="pb-10"
+        responsive={responsive}
+        showDots={true}
+        // autoPlay={true}
+        // autoPlaySpeed={1000}
+        infinite={true}
+        ssr={true}
+        // partialVisbile={true}
+        centerMode={true}
+        // focusOnSelect={true}
+      >
+        {CAROUSEL_DATA.map((item) => (
+          <div key={item.name} className="mx-1">
+            <Image
+              src={item.img}
+              width={200}
+              height={200}
+              alt={item.name}
+              className="border-4 border-blue-50 rounded-full"
+            />
+          </div>
+        ))}
+      </Carousel>
+    </div>
+  );
+};
 
-// export default CarouselItem;
+export default CarouselItem;
