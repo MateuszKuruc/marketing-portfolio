6d6f4df4b7fcafac50ef86a2b24709fa05a3ad56
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-10-30 21:48:07 +0100

    clean code in contact component

--- marketing-portfolio/src/components/Contact.tsx
+++ marketing-portfolio/src/components/Contact.tsx
@@ -10,15 +10,13 @@ const Contact = () => {
       className="border-t-4 py-0 max-container bg-radial-gradient"
     >
       <div className="flex flex-col relative w-full md:items-center">
-        {/* <div className="flex flex-col items-center justify-center"> */}
-
         <h1 className="self-center bold-52 mt-8 lg:bold-70 text-blue-50">
           Kontakt
         </h1>
         <p className="p-4 regular-20 text-white ">
           Uzupelnij formularz i daj znac czego potrzebujesz
         </p>
-        {/* </div> */}
+
         <Image
           src="/background.jpg"
           className="w-full h-full absolute object-cover mix-blend-luminosity rounded-2xl opacity-5"
