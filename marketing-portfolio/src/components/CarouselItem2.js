993c7aebfd7931d62adaba490851db8aa10cc266
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-10-30 16:18:36 +0100

    add basic styling for carousel

--- marketing-portfolio/src/components/CarouselItem2.js
+++ marketing-portfolio/src/components/CarouselItem2.js
@@ -2,53 +2,51 @@
 import React from "react";
 import { CAROUSEL_DATA } from "@/constants";
 
-
-
-import Carousel from 'react-multi-carousel';
-import 'react-multi-carousel/lib/styles.css';
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 import Image from "next/image";
 
 // import Image from "next/image";
 
-
-
 const CarouselItem2 = () => {
-    const responsive = {
-        superLargeDesktop: {
-          // the naming can be any, depends on you.
-          breakpoint: { max: 4000, min: 3000 },
-          items: 5
-        },
-        desktop: {
-          breakpoint: { max: 3000, min: 1024 },
-          items: 3
-        },
-        tablet: {
-          breakpoint: { max: 1024, min: 464 },
-          items: 2
-        },
-        mobile: {
-          breakpoint: { max: 464, min: 0 },
-          items: 1
-        }
-      };
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
 
-
-    return (
-<div className="max-container padding-container"><h1>test</h1>
-<Carousel responsive={responsive}>
-    {CAROUSEL_DATA.map(item => (
-        <div key={item.name}>
-<Image src={item.img} width={200} height={200} alt={item.name} />
-        </div>
-    ))}
-  {/* <div>Item 1</div>
+  return (
+    <div 
+    className="max-container padding-container"
+    >
+      <h1 className="text-blue-50 bold-52 lg:bold-70">Klienci, którzy mi zaufali:</h1>
+      <Carousel responsive={responsive}>
+        {CAROUSEL_DATA.map((item) => (
+          <div key={item.name}>
+            <Image src={item.img} width={200} height={200} alt={item.name} className="border-4 rounded-2xl border-blue-50" />
+          </div>
+        ))}
+        {/* <div>Item 1</div>
   <div>Item 2</div>
   <div>Item 3</div>
   <div>Item 4</div> */}
-</Carousel>
-</div>
-    )
-}
+      </Carousel>
+    </div>
+  );
+};
 
 export default CarouselItem2;
