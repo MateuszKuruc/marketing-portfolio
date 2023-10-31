93b12a550b4257c73b604bc5ddbc30419dac0e1f
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-10-31 12:22:24 +0100

    implement conditionally rendered mobile menu based on state management

--- marketing-portfolio/src/components/NavBar.tsx
+++ marketing-portfolio/src/components/NavBar.tsx
@@ -8,6 +8,7 @@ import { AiOutlineMenu } from "react-icons/ai";
 import { Link as ScrollLink } from "react-scroll";
 
 const NavBar = () => {
+  const [showMobileMenu, setShowMobileMenu] = useState(true);
   const [activeLink, setActiveLink] = useState(() => {
     return localStorage.getItem("activeLink") || "home";
   });
@@ -56,35 +57,70 @@ const NavBar = () => {
   });
 
   return (
-    <nav className="opacity-95 sticky top-0 border-2 border-black flexAround glob max-container padding-container z-30 py-8 bg-gray-90">
-      <Link href="/">
-        <Image src="/boostguru.png" alt="home_logo" width={70} height={46} />
-      </Link>
-      <ul className="hidden h-full gap-12 lg:flex">
-        {NAV_LINKS.map((link) => (
-          <li key={link.key}>
-            <ScrollLink
-              to={link.href}
-              spy={true}
-              smooth={true}
-              offset={-85}
-              duration={700}
-              className={`regular-16 text-white flexCenter cursor-pointer pb-1.5 hover:font-bold ${
-                activeLink === link.key ? "border-b-3 border-purple-500" : ""
-              }`}
-              onClick={() => {
-                autoScrolling.current = true;
-                handleLinkClick(link.key);
-              }}
-            >
-              {link.label}
-            </ScrollLink>
-          </li>
-        ))}
-      </ul>
+    <section className="bg-gray-90 sticky top-0 z-30">
+      <nav className="opacity-95 sticky top-0 border-2 border-black flexAround glob max-container padding-container py-8 bg-gray-90">
+        <Link href="/">
+          <Image src="/boostguru.png" alt="home_logo" width={70} height={46} />
+        </Link>
+        <ul className="hidden h-full gap-12 lg:flex">
+          {NAV_LINKS.map((link) => (
+            <li key={link.key}>
+              <ScrollLink
+                to={link.href}
+                spy={true}
+                smooth={true}
+                offset={-85}
+                duration={700}
+                className={`regular-16 text-white flexCenter cursor-pointer pb-1.5 hover:font-bold ${
+                  activeLink === link.key ? "border-b-3 border-purple-500" : ""
+                }`}
+                onClick={() => {
+                  autoScrolling.current = true;
+                  handleLinkClick(link.key);
+                }}
+              >
+                {link.label}
+              </ScrollLink>
+            </li>
+          ))}
+        </ul>
 
-      <AiOutlineMenu color="white" className="bold-32 lg:hidden" />
-    </nav>
+        <AiOutlineMenu
+          color="white"
+          onClick={() => setShowMobileMenu(!showMobileMenu)}
+          className="bold-32 lg:hidden"
+        />
+      </nav>
+      {showMobileMenu && (
+        <div className="bg-blue-50 p-10 flex flex-col">
+          <ul className="h-full gap-12">
+            {NAV_LINKS.map((link) => (
+              <li key={link.key}>
+                <ScrollLink
+                  to={link.href}
+                  spy={true}
+                  smooth={true}
+                  offset={-85}
+                  duration={700}
+                  className={`medium-18 text-black flexCenter cursor-pointer pb-1.5 hover:font-bold ${
+                    activeLink === link.key
+                      ? "border-b-3 border-purple-500"
+                      : ""
+                  }`}
+                  onClick={() => {
+                    autoScrolling.current = true;
+                    handleLinkClick(link.key);
+                    setShowMobileMenu(!showMobileMenu);
+                  }}
+                >
+                  {link.label}
+                </ScrollLink>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </section>
   );
 };
 
