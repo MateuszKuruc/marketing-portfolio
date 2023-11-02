c159e1fd44baad0d7d641ec5c12aaa43627fe8cd
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-11-02 17:52:51 +0100

    style hero section for mobile and desktop screens, make changes to about section

--- marketing-portfolio/src/components/About.tsx
+++ marketing-portfolio/src/components/About.tsx
@@ -7,28 +7,37 @@ const About = () => {
       id="about"
       className="border-t-4 border-white my-10 skew-y-3 bg-gradient-to-b from-gray-90 max-container padding-container py-10 md:gap-28 lg:py-20 gap-20"
     >
-      <div className="-skew-y-3 flex gap-10 ">
+      <div className="-skew-y-3 flex gap-10 justify-around">
         <header>
-          <h1 className="bold-52 lg:bold-70 text-blue-50">O mnie</h1>
-          <p className="regular-20 mt-6 xl:max-w-[520px] text-white">
+          <h1 className="bold-64 lg:bold-88 text-blue-50">O mnie</h1>
+          <p className="regular-24 mt-6 xl:max-w-[520px] text-white">
             Jestem fajowym marketingowcem, zrobie ci graficzke i zmontuje teksty
             reklamowe. Zajmuje sie tym 20 lat co najmniej. Co bys jeszcze
             chcial? Robie:
           </p>
-          <ul className="list-disc text-white regular-20">
+          <ul className="list-disc text-white regular-24">
             <li className="mt-3">kampanie fb</li>
             <li>prowadze social media fb i ig</li>
             <li>prowadze tysiac rodzajow kampanii w necie</li>
             <li>kompleksowo ogarne ci marketing</li>
           </ul>
         </header>
+        <div className="flex flex-col gap-6 items-center justify-center">
+
         <Image
           className="hidden lg:block"
-          src="/placeholder.jpg"
-          width={350}
+          src="/meta_cert.png"
+          width={250}
           height={350}
-          alt="marketing photo"
-        />
+          alt="Meta certification"
+          />
+        <Image
+          src="/google_cert.png"
+          width={250}
+          height={350}
+          alt="Google certification"
+          />
+          </div>
       </div>
     </section>
   );
--- marketing-portfolio/src/components/Hero.tsx
+++ marketing-portfolio/src/components/Hero.tsx
@@ -6,7 +6,9 @@ const Hero = () => {
       id="hero"
       className="border-b-4 border-white skew-y-3 bg-gradient-to-t from-gray-90 max-container padding-container flex flex-col flexCenter gap-20 py-10 pb-32 md:gap-28 lg:py-20"
     >
-      <div className="-skew-y-3 lg:flex gap-10 border-5 border-slate-200">
+      <div className="-skew-y-3 gap-10 border-5 border-slate-200 flex flex-col-reverse lg:flex-row">
+        <div className="lg:ml-12">
+
         <header>
           <h1 className="mt-6 bold-64 lg:bold-88 text-blue-50">
             Social media ekspert
@@ -17,6 +19,7 @@ const Hero = () => {
             na dole, tutaj tylko wstep. Bedzie spoko strona z tego.
           </p>
         </header>
+        </div>
         <Image
           src="/bartosz_kuruc.png"
           width={500}
