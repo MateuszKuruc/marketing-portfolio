d9344e009be0f0bb30bbdad58ed3e01fdad0ea17
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-11-03 00:38:23 +0100

    improve styling of portfolio component for mobile

--- marketing-portfolio/src/components/Portfolio.tsx
+++ marketing-portfolio/src/components/Portfolio.tsx
@@ -20,14 +20,10 @@ const Portfolio = () => {
       id="portfolio"
       className=" border-t-4 border-white max-container padding-container bg-gradient-to-t from-gray-30 to-gray-90 py-10 lg:py-20 md:gap-28"
     >
-      <header className="mb-10 padding container max-container flex flex-col items-center px-24">
+      <header className="mb-10 padding container max-container flex flex-col items-center">
         <h1 className="font-header bold-40 lg:bold-52 text-blue-50">
           Case study
         </h1>
-        {/* <p className="regular-24 text-white mt-6">
-          Sprawdz jak poprawilem marketingowe wskazniki u moich klientow i
-          wynioslem ich marketing na jakis kosmiczny poziom ze hej
-        </p> */}
       </header>
       <div className="max-container md:padding-container">
         {ACCORDION_DATA.map((data, index) => {
