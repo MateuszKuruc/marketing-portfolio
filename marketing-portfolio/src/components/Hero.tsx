2801b778fad3ce9fe9f60ddf29c58e1e393f2937
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-11-02 21:02:48 +0100

    change styling for hero section

--- marketing-portfolio/src/components/Hero.tsx
+++ marketing-portfolio/src/components/Hero.tsx
@@ -4,7 +4,7 @@ const Hero = () => {
   return (
     <section
       id="hero"
-      className="border-b-4 border-white skew-y-3 bg-gradient-to-b from-gray-90 to-coral-50 max-container padding-container flex flex-col flexCenter gap-20 py-10 pb-32 md:gap-28 lg:py-20"
+      className="border-b-4 border-white skew-y-3 bg-gradient-to-t from-gray-90 to-gray-30 max-container padding-container flex flex-col flexCenter gap-20 py-10 pb-32 md:gap-28 lg:py-20"
     >
       <div className="-skew-y-3 gap-10 border-5 border-slate-200 flex flex-col-reverse lg:flex-row">
         <div className="lg:ml-12">
@@ -12,9 +12,9 @@ const Hero = () => {
             <h1 className="font-header hidden md:block mt-6 bold-40 lg:bold-52 text-blue-50">
               Facebook Ads - razem zrobimy z tego hit!
             </h1>
-            <p className="medium-22 mt-6 text-black xl:max-w-[520px]">
+            <p className="font-standard medium-22 mt-6 text-white xl:max-w-[520px]">
               Nie wiesz, jak zdominować świat{" "}
-              <span className="text-white">Facebook Ads</span>? Trafiłeś w
+              <strong>Facebook Ads</strong>? Trafiłeś w
               dziesiątkę! Odkryj strategie mistrza reklam, które przekształcą
               Twoje kampanie w sukcesy.{" "}
               <span className="text-white">
