c615332ebb5e13485f8f127af4c65a4ed5bb3326
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-11-02 23:34:50 +0100

    minor style adjustments for hero section

--- marketing-portfolio/src/components/Hero.tsx
+++ marketing-portfolio/src/components/Hero.tsx
@@ -4,15 +4,15 @@ const Hero = () => {
   return (
     <section
       id="hero"
-      className="border-b-4 border-white skew-y-3 bg-gradient-to-t from-gray-90 to-gray-30 max-container padding-container flex flex-col flexCenter gap-20 py-10 pb-32 md:gap-28 lg:py-20"
+      className="border-b-4 border-white skew-y-3 bg-gradient-to-t from-gray-90 to-gray-30 max-container padding-container flex flex-col flexCenter xs:py-5 md:py-10 md:gap-28 lg:py-20"
     >
-      <div className="-skew-y-3 gap-10 border-5 border-slate-200 flex flex-col-reverse lg:flex-row">
+      <div className="-skew-y-3 md:gap-10 border-5 border-slate-200 flex flex-col-reverse lg:flex-row">
         <div className="lg:ml-12">
           <header>
-            <h1 className="font-header hidden lg:block mt-6 bold-40 lg:bold-52 text-blue-50">
+            <h1 className="font-header hidden lg:block bold-40 lg:bold-52 text-blue-50">
               Facebook Ads - razem zrobimy z tego hit!
             </h1>
-            <p className="font-standard regular-20 mt-6 text-white xl:max-w-[520px]">
+            <p className="font-standard regular-20 my-4 text-white xl:max-w-[520px]">
               Nie wiesz, jak zdominować świat{" "}
               <span className="font-medium">Facebook Ads</span>? Trafiłeś w
               dziesiątkę! Odkryj strategie mistrza reklam, które przekształcą
