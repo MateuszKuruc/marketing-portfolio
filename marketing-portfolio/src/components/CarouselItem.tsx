1da80606266129da338b082d057e0c27a49d3300
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-11-02 18:22:12 +0100

    carousel items display adjusted

--- marketing-portfolio/src/components/CarouselItem.tsx
+++ marketing-portfolio/src/components/CarouselItem.tsx
@@ -10,11 +10,11 @@ const CarouselItem = () => {
   const responsive = {
     superLargeDesktop: {
       breakpoint: { max: 4000, min: 1024 },
-      items: 5,
+      items: 4,
     },
     desktop: {
       breakpoint: { max: 1024, min: 800 },
-      items: 5,
+      items: 4,
     },
     tablet: {
       breakpoint: { max: 800, min: 464 },
@@ -22,17 +22,14 @@ const CarouselItem = () => {
     },
     mobile: {
       breakpoint: { max: 464, min: 0 },
-      items: 3,
+      items: 2,
     },
   };
 
   return (
     <div className="max-container padding-container bg-pink-50 p-10">
       <div className="flex items-center justify-center mb-12">
-
-      <h1 className="text-blue-50 bold-64 lg:bold-88">
-        Zaufali mi:
-      </h1>
+        <h1 className="text-blue-50 bold-64 lg:bold-88">Zaufali mi:</h1>
       </div>
       <Carousel
         className="pb-10"
@@ -51,8 +48,8 @@ const CarouselItem = () => {
           <div key={item.name} className="mx-1">
             <Image
               src={item.img}
-              width={200}
-              height={200}
+              width={300}
+              height={300}
               alt={item.name}
               className="border-4 border-blue-50 rounded-full"
             />
