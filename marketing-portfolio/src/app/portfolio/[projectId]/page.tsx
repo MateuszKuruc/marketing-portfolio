f6a5ff6c2f74f7406f12780290aebb901ed5806e
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-11-03 15:34:23 +0100

    fix data display for case study nr 1

--- marketing-portfolio/src/app/portfolio/[projectId]/page.tsx
+++ marketing-portfolio/src/app/portfolio/[projectId]/page.tsx
@@ -16,7 +16,7 @@ const id = params.projectId;
    {CLIENT_DATA.map(client => client.id.toString() === id ? (
     <>
     
-    <h1 className="text-blue-50 bold-52 lg:bold-70 mb-4">{client.clientName}</h1>
+    <h1 className="text-blue-50 bold-52 lg:bold-70 mb-4">{client.title}</h1>
     <p className="text-white regular-20">{client.shortDescription}</p>
     <h2 className="text-blue-50 bold-40 lg:bold-52 my-4">Wyniki</h2>
     <p className="text-white regular-20">{client.results}</p>
