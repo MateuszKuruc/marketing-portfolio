33ee44e8afaa8992d919d441e9d3749a8dd692c2
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-11-07 11:08:44 +0100

    change accordion rendering to improve accessibility by hiding focusable elements when accordion is collapsed

--- marketing-portfolio/src/components/AccordionItem.tsx
+++ marketing-portfolio/src/components/AccordionItem.tsx
@@ -17,7 +17,7 @@ const AccordionItem = ({
         className="bg-gray-90 border-2 p-10 flex flexBetween border-coral-50"
         onClick={toggleAccordion}
       >
-        <p className="font-header text-white bold-32">{title}</p>
+        <p className="font-header text-white regular-32">{title}</p>
         <div>
           {open ? (
             <AiFillMinusCircle color="white" size={50} />
@@ -30,12 +30,15 @@ const AccordionItem = ({
       <Collapse className="ReactCollapse--collapse" isOpened={open}>
         <div className="font-standard regular-20 p-5 flex flex-col gap-4">
           <p>{description}</p>
-          <Link
+          {open && (
+
+            <Link
             className="font-medium text-slate-500 regular-20 hover:text-slate-700"
             href={`/portfolio/${id}`}
-          >
+            >
             Zobacz więcej...
           </Link>
+            )}
         </div>
       </Collapse>
     </div>
